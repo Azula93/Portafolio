8def5e7a63b5020c7d0eeff60d7c0ccb867fa82f
Azula Dev <caligeekweb@gmail.com>
2024-01-20 16:30:09 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import NavBar from './components/navBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <NavBar/>
     </div>
   );
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,39 +1,67 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import logo from  '../assets/img/logoazula2.jpg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import '../styles/navbar.css';
 
-function BasicExample() {
+function NavBar() {
+
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () =>{
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      }else{
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) =>{
+    setActiveLink(value);
+  }
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}> 
       <Container>
         <Navbar.Brand href="#home">
-        <img src={''} alt="Logo" />
+        <img src={logo} alt="Logo" />
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" > 
-          <span className></span>
-        <Navbar.Toggle/>
+          <span className='navbar-toggler-icon' ></span>
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home') }>Home</Nav.Link>
+            <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')} >Skills</Nav.Link>
+            <Nav.Link href="#project" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Project</Nav.Link>
           </Nav>
+          <span className='navbar-text'>
+            <div className="social-icon">
+              <Link> <img src={navIcon1} alt="" /> </Link>
+              <Link> <img src={navIcon2} alt="" /> </Link>
+              <Link> <img src={navIcon3} alt="" /> </Link>
+            </div>
+            <button className='vvd'onClick={() => console.log('connect') } >
+              <span>Contactame</span>
+            </button>
+          </span>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default BasicExample;
+export default NavBar;
